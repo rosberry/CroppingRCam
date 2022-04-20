46271d1573798b05eb550eeef8eaf8e9563b0eba
exxcqz <electro.style72@gmail.com>
2022-04-20 12:31:22 +0600

    Add dependencies to CroppingRCam

--- Package.swift
+++ Package.swift
@@ -7,10 +7,26 @@ let package = Package(
     name: "CroppingRCam",
     platforms: [.iOS(.v11)],
     products: [
-        .library(name: "CroppingRCam", targets: ["CroppingRCam"]),
+        .library(
+            name: "CroppingRCam",
+            targets: ["CroppingRCam"]),
     ],
+    dependencies: [
+            // Dependencies declare other packages that this package depends on.
+            // .package(url: /* package url */, from: "1.0.0"),
+            .package(url: "https://github.com/rosberry/rcam",
+                        .branch("master")),
+            .package(url: "https://github.com/rosberry/Cripper",
+                        .branch("master")),
+            .package(url: "https://github.com/rosberry/Bin-iOS",
+                        .branch("no-xcframeworks")),
+        ],
     targets: [
-        .target(name: "CroppingRCam", path: "CroppingRCam/Sources"),
-        .testTarget(name: "CroppingRCamTests", dependencies: ["CroppingRCam"], path: "CroppingRCam/Tests")
+        .target(name: "CroppingRCam",
+                dependencies: ["Rcam", "Cripper"],
+                path: "CroppingRCam/Sources"),
+        .testTarget(name: "CroppingRCamTests",
+                    dependencies: ["CroppingRCam"],
+                    path: "CroppingRCam/Tests")
     ]
 )
