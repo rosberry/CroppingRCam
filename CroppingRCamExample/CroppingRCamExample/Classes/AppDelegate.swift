c8198db477117ee6052b531702ec7c4753abfc54
exxcqz <electro.style72@gmail.com>
2022-04-20 12:59:08 +0600

    Add example project

---
+++ CroppingRCamExample/CroppingRCamExample/Classes/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CroppingRCamExample
+//
+//  Created by Nikita Gavrikov on 20.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    weak var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
